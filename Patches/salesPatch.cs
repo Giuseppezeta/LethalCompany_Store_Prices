b49fca30c7fb3c90e1cd659fc3d189457dab2c37
Giuse <Giuse@GIUSEPPEZETA>
2023-12-09 03:17:02 +0100

    Aggiungere i file di progetto.

---
+++ Patches/salesPatch.cs
@@ -0,0 +1,150 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LethalCompanyPluginTemplate.Patches
+{
+    [HarmonyPatch(typeof(Terminal))]
+    internal class salesPatch
+    {
+        [HarmonyPatch(nameof(Terminal.SetItemSales))]
+        [HarmonyPostfix()]
+
+        static void StorePrices(ref Item[] ___buyableItemsList)
+        {
+            ___buyableItemsList[0].creditsWorth = Plugin.WalkieTalkie.Value;
+            ___buyableItemsList[1].creditsWorth = Plugin.Flashlight.Value;
+            ___buyableItemsList[2].creditsWorth = Plugin.Shovel.Value;
+            ___buyableItemsList[3].creditsWorth = Plugin.Lockpicker.Value;
+            ___buyableItemsList[4].creditsWorth = Plugin.ProFlashlight.Value;
+            ___buyableItemsList[5].creditsWorth = Plugin.Stun_granade.Value;
+            ___buyableItemsList[6].creditsWorth = Plugin.Boombox.Value;
+            ___buyableItemsList[7].creditsWorth = Plugin.TZP.Value;
+            ___buyableItemsList[8].creditsWorth = Plugin.Zap_gun.Value;
+            ___buyableItemsList[9].creditsWorth = Plugin.Jetpack.Value;
+            ___buyableItemsList[10].creditsWorth = Plugin.Extension_ladder.Value;
+            ___buyableItemsList[11].creditsWorth = Plugin.Radar_booster.Value;
+            //___buyableItemsList[12].creditsWorth = Plugin.Loud_horn.Value;
+            //___buyableItemsList[13].creditsWorth = Plugin.Teleporter.Value;
+            //___buyableItemsList[14].creditsWorth = Plugin.Inverse_Teleporter.Value;
+        }
+    }
+
+    public class Item : ScriptableObject
+    {
+        public int itemId;
+
+        public string itemName;
+
+        [Space(3f)]
+        public List<ItemGroup> spawnPositionTypes = new List<ItemGroup>();
+
+        [Space(3f)]
+        public bool twoHanded;
+
+        public bool twoHandedAnimation;
+
+        public bool canBeGrabbedBeforeGameStart;
+
+        [Space(3f)]
+        public float weight = 1f;
+
+        public bool itemIsTrigger;
+
+        public bool holdButtonUse;
+
+        public bool itemSpawnsOnGround = true;
+
+        [Space(5f)]
+        public bool isConductiveMetal;
+
+        [Header("Scrap-collection")]
+        public bool isScrap;
+
+        public int creditsWorth;
+
+        public bool lockedInDemo;
+
+        public int highestSalePercentage = 80;
+
+        [Space(3f)]
+        public int maxValue;
+
+        public int minValue;
+
+        public GameObject spawnPrefab;
+
+        [Space(3f)]
+        [Header("Battery")]
+        public bool requiresBattery = true;
+
+        public float batteryUsage = 15f;
+
+        public bool automaticallySetUsingPower = true;
+
+        [Space(5f)]
+        public Sprite itemIcon;
+
+        [Space(5f)]
+        [Header("Player animations")]
+        public string grabAnim;
+
+        public string useAnim;
+
+        public string pocketAnim;
+
+        public string throwAnim;
+
+        [Space(5f)]
+        public float grabAnimationTime;
+
+        [Header("Player SFX")]
+        public AudioClip grabSFX;
+
+        public AudioClip dropSFX;
+
+        public AudioClip pocketSFX;
+
+        public AudioClip throwSFX;
+
+        [Header("Netcode")]
+        public bool syncGrabFunction = true;
+
+        public bool syncUseFunction = true;
+
+        public bool syncDiscardFunction = true;
+
+        public bool syncInteractLRFunction = true;
+
+        [Header("Save data")]
+        public bool saveItemVariable;
+
+        [Header("MISC")]
+        public bool isDefensiveWeapon;
+
+        [Space(3f)]
+        public string[] toolTips;
+
+        public float verticalOffset;
+
+        public int floorYOffset;
+
+        public bool allowDroppingAheadOfPlayer = true;
+
+        public Vector3 restingRotation = new Vector3(0f, 0f, 90f);
+
+        public Vector3 rotationOffset = Vector3.zero;
+
+        public Vector3 positionOffset = Vector3.zero;
+
+        public bool meshOffset = true;
+
+        public Mesh[] meshVariants;
+
+        public Material[] materialVariants;
+
+        public bool usableInSpecialAnimations;
+
+        public bool canBeInspected = true;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using HarmonyLib;
+using LethalCompanyPluginTemplate.Patches;
+using BepInEx.Configuration;
+
+namespace LethalCompanyPluginTemplate
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance;
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        //config variables
+        public static ConfigEntry<int> WalkieTalkie;
+        public static ConfigEntry<int> Flashlight;
+        public static ConfigEntry<int> Shovel;
+        public static ConfigEntry<int> Lockpicker;
+        public static ConfigEntry<int> ProFlashlight;
+        public static ConfigEntry<int> Stun_granade;
+        public static ConfigEntry<int> Boombox;
+        public static ConfigEntry<int> TZP;
+        public static ConfigEntry<int> Zap_gun;
+        public static ConfigEntry<int> Jetpack;
+        public static ConfigEntry<int> Extension_ladder;
+        public static ConfigEntry<int> Radar_booster;
+        //public static ConfigEntry<int> Loud_horn;
+        //public static ConfigEntry<int> Teleporter;
+        //public static ConfigEntry<int> Inverse_Teleporter;
+        private void Awake()
+        {
+            // Plugin startup logic
+            if (Plugin.Instance == null) { Plugin.Instance = this; };
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loading!");
+            this.LoadConfigs();
+            harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(salesPatch));
+        }
+        private void LoadConfigs()
+        {
+            WalkieTalkie = Config.Bind("Settings", "WalkieTalkie", 12, "");
+            Flashlight = Config.Bind("Settings", "Flashlight", 15, "");
+            Shovel = Config.Bind("Settings", "Shovel", 30, "");
+            Lockpicker = Config.Bind("Settings", "Lockpicker", 20, "");
+            ProFlashlight = Config.Bind("Settings", "ProFlashlight", 25, "");
+            Stun_granade = Config.Bind("Settings", "Stun granade", 40, "");
+            Boombox = Config.Bind("Settings", "Boombox", 60, "");
+            TZP = Config.Bind("Settings", "TZP", 120, "");
+            Zap_gun = Config.Bind("Settings", "Zap gun", 400, "");
+            Jetpack = Config.Bind("Settings", "Jetpack", 700, "");
+            Extension_ladder = Config.Bind("Settings", "Extension ladder", 60, "");
+            Radar_booster = Config.Bind("Settings", "Radar booster", 50, "");
+            //Loud_horn = Config.Bind("Settings", "Loud horn", 150, "");
+            //Teleporter = Config.Bind("Settings", "Teleporter", 375, "");
+            //Inverse_Teleporter = Config.Bind("Settings", "Inverse Teleporter", 425, "");
+        }
+    }
+}
